acbf14c9c2f032e6c96ee7fc8ae4ce9e731808a3
Kinga Jablonska <k.a.jablonska93@gmail.com>
2022-09-01 19:58:31 +0200

    Add new file

--- Zadanie2/Zadanie2.cpp
+++ Zadanie2/Zadanie2.cpp
@@ -1,16 +1,22 @@
-// Zadanie 2
-//Napisz funkcjê, która przyjmie dwa stringi i zwróci ile liter maj¹ wspólnych.
-
-
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
+    string first = "minion";
+    string second = "banana";
+    int count = 0;
 
-    string first;
-    string second;
+    for (int i = 0; i < first.size(); i++)
+    {
+        if (first[i] == second[i])
 
-    
+        {
+            count++;
+        }
+
+    }
+    cout << count;
+
 }
-
